948ff1eaab0292d0f54061b5b1dc82d2e9affe87
StupidRaRa <sa25168693@gmail.com>
2023-10-27 22:39:36 +0800

    feat:user stop when touch flag, and add choose item UI

--- Assets/Scripts/LinBenScript.cs
+++ Assets/Scripts/LinBenScript.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LinBenScript : MonoBehaviour
 {
+    [Header("Custom Event")]
+    public UnityEvent myEvents;
     private Animator anim;
     private Transform pos;
     // Start is called before the first frame update
@@ -26,5 +29,23 @@ public class LinBenScript : MonoBehaviour
             Debug.Log(pos.position.x);
             pos.position = pos.position + new Vector3((float) 0.1, 0, 0);
         }*/
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Vector3 targetPosition = other.transform.position;
+            targetPosition.y = transform.position.y; // Maintain the same Y level
+            transform.LookAt(targetPosition);
+
+            if (myEvents == null) {
+                print("myEventTriggerOnEnter was triggered but myEvents was null");
+            }
+            else {
+                print("myEventTriggerOnEnter Activated. Triggering" + myEvents);
+                myEvents.Invoke();
+            }
+            other.GetComponent<Player>().enabled = false;
+        }
     }
 }
