023efb44505d9030683487540eaaaecee34fd320
tooooooot <linyugung@gmail.com>
2023-12-28 21:38:10 +0800

    feat: add particle on coin

--- Assets/Scripts/Items/Coin/TriggerCoin.cs
+++ Assets/Scripts/Items/Coin/TriggerCoin.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class TriggerCoin : MonoBehaviour
 {
+    private Transform parent;
+
+    void Awake() {
+        parent = transform.parent;
+    }
+
     private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Player")) {
-            transform.parent.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-            transform.parent.SetParent(other.transform);
-            transform.parent.SetLocalPositionAndRotation(new Vector3(0f, 2.7f, 0f), Quaternion.Euler(0, 0, 0));
+        if (other.gameObject.CompareTag("Player") && parent.GetComponent<Coin>().IsRotate()) {
+            parent.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            parent.SetParent(other.transform);
+            parent.SetLocalPositionAndRotation(new Vector3(0f, 2.7f, 0f), Quaternion.Euler(0, 0, 0));
             GetComponent<MeshCollider>().enabled = false;
         }
     } 
