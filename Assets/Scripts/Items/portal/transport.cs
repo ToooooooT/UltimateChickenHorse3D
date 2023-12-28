9bbf5ca96c35ad974f6346c4b61939717a46b473
tooooooot <linyugung@gmail.com>
2023-12-28 23:57:06 +0800

    fix: add portal particle system

--- Assets/Scripts/Items/portal/transport.cs
+++ Assets/Scripts/Items/portal/transport.cs
@@ -2,40 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class transport : MonoBehaviour
+public class Transport : MonoBehaviour
 {
-    public enum State { enable, unable };
-    public State state; 
-    public float countdown;
-    public ParticleSystem particleSystem;
+    private enum State { enable, unable };
+    private State state; 
+
+    private float countdown;
+    private GameObject magic;
     private GameObject[] portals;
-    private Color invalidColor = new(1.0f, 0.0f, 0.0f, 0.05f);
     private Color validColor = new(0.0f, 1.0f, 0.0f, 0.05f);
 
-    void Start()
-    {
+    void Start() {
         state = State.unable;
+        magic = transform.Find("Magic").gameObject;
         countdown = 0;
     }
 
-    void Update()
-    {
+    void Update() {
         portals = GameObject.FindGameObjectsWithTag("portal");
         if (portals.Length < 2) {
             state = State.unable;
             countdown = 0;
-        }
-        else if(countdown <= 0) {
+        } else if(countdown <= 0) {
             if (state == State.unable) {
                 state = State.enable;
                 Renderer cubeRenderer = GetComponent<Renderer>();
                 int newLayerMask = 1 << 1;
                 cubeRenderer.renderingLayerMask = (uint)newLayerMask;
-                if (this.TryGetComponent<Renderer>(out var renderer)) {
+                if (TryGetComponent<Renderer>(out var renderer)) {
                     renderer.material.color = validColor;
                 }
-            }
-            else if (state == State.enable) {
+            } else if (state == State.enable) {
                 state = State.unable;
                 Renderer cubeRenderer = GetComponent<Renderer>();
                 int newLayerMask = 0;
@@ -45,17 +42,17 @@ public class transport : MonoBehaviour
         }
         countdown -= 0.01f;
     }
-    public void StartParticleSystem()
-    {
-        particleSystem.Play();
+
+    public void StartParticleSystem() {
+        magic.SetActive(true);
     }
-    private void OnTriggerEnter(Collider other)
-    {
+
+    private void OnTriggerEnter(Collider other) {
         portals = GameObject.FindGameObjectsWithTag("portal");
-        List<GameObject> enablePortals = new List<GameObject>();
-        for (int i=0;i<portals.Length;i++) {
-            if (portals[i] != this.gameObject) {
-                transport portalScript = portals[i].GetComponent<transport>();
+        List<GameObject> enablePortals = new();
+        for (int i = 0; i < portals.Length; i++) {
+            if (portals[i] != gameObject) {
+                Transport portalScript = portals[i].GetComponent<Transport>();
                 if (portalScript != null && portalScript.state == State.enable) {
                     enablePortals.Add(portals[i]);
                 }
@@ -63,7 +60,7 @@ public class transport : MonoBehaviour
         }
         if(enablePortals.Count > 0) {
             int choose = Random.Range(0, enablePortals.Count);
-            transport chosenPortalScript = enablePortals[choose].GetComponent<transport>();
+            Transport chosenPortalScript = enablePortals[choose].GetComponent<Transport>();
             
             chosenPortalScript.state = State.unable;
             chosenPortalScript.countdown = Random.Range(8f, 10f);
@@ -82,8 +79,7 @@ public class transport : MonoBehaviour
             if (other.CompareTag("Player")) {
                 Player otherscript = other.GetComponent<Player>();
                 otherscript.ModifyPosition(enablePortals[choose].transform.position);
-            }
-            else if(!other.CompareTag("Wall")) {
+            } else if(!other.CompareTag("Wall")) {
                 other.transform.position = enablePortals[choose].transform.position;
             }
             
